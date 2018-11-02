c0d41a520e15252f1973e76d6488995db5de5f89
zbych89 <zbych89@gmail.com>
2018-11-02 23:00:33 +0100

    10.1 Upd

--- kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -1,6 +1,6 @@
 package com.kodilla.spring.forum;
 
-import com.kodilla.spring.SpringRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ForumUserTestSuite{
     @Test
