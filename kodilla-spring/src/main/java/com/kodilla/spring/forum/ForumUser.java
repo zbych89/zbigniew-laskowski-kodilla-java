654d5bdd085a9e65eb6524f8d70f33cfc762b8aa
zbych89 <zbych89@gmail.com>
2018-11-02 22:28:23 +0100

    10.1 Spring

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -0,0 +1,15 @@
+package com.kodilla.spring.forum;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ForumUser {
+    private String userName;
+    public ForumUser(){
+        this.userName = "John Smith";
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
---
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -0,0 +1,20 @@
+package com.kodilla.spring.forum;
+
+import com.kodilla.spring.SpringRunner;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+public class ForumUserTestSuite{
+    @Test
+    public void testGetUsername(){
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        ForumUser user = context.getBean(ForumUser.class);
+        String name = user.getUserName();
+        Assert.assertEquals("John Smith",name);
+    }
+}
