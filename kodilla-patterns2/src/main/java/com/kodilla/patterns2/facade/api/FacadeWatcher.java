1c0066f92ad95e1b20f51d568b668130a799f1c8
zbych89 <zbych89@gmail.com>
2019-01-08 00:10:49 +0100

    20.3 Watcher

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/FacadeWatcher.java
@@ -0,0 +1,20 @@
+package com.kodilla.patterns2.facade.api;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+
+
+@Aspect
+@Component
+public class FacadeWatcher {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FacadeWatcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public void logEvent(){
+        LOGGER.info("An order is being processed");
+    }
+}
